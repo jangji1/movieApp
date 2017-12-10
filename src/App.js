6a32a2987b1fe0d05dcc01376c7e2b49b7e388df
jangji1 <jangji1@gmail.com>
2017-12-10 23:57:19 +0900

    Async Await 추가

--- src/App.js
+++ src/App.js
@@ -10,20 +10,31 @@ class App extends Component {
   state = {
 
   }
-  
+
   componentDidMount(){
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
-    })
+    const movies = this.state.movies.map(movie => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
+    }) // 컴포넌트의 key는 인덱스를 사용하는 것은 좋지 않다. 느리다.
 
     return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    }) // movies: movies 와 같음
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies) // arrow function은 return을 할 필요 없음. return 뜻이 내재되어 있음
+    .catch(err => console.log(err))
   }
 
   render() {
