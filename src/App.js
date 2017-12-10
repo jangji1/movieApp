626fb68c5e17a07f10787a4550bccfcd6fc40e7e
jangji1 <jangji1@gmail.com>
2017-12-10 23:06:34 +0900

    AJAX 추가

--- src/App.js
+++ src/App.js
@@ -8,35 +8,14 @@ class App extends Component {
   // update componentWillReceiveProps() -> sholdComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
   state = {
-  }
 
+  }
+  
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
-          },
-          {
-            title: "Full Metal Jacket",
-            poster: "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
-          },
-          {
-            title: "Oldboy",
-            poster: "https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg"
-          },
-          {
-            title: "Star Wars",
-            poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/220px-Star_Wars_Logo.svg.png"
-          },
-          {
-            title: "Trainspotting",
-            poster: "https://upload.wikimedia.org/wikipedia/en/7/71/Trainspotting_ver2.jpg"
-          }
-        ]
-      })
-    }, 5000)
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
   }
 
   _renderMovies = () => {
